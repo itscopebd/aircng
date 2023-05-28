cff2e2463cc5d9935da9eebf20a9100c1bf5d473
itscopebd <itscopebd@gmail.com>
2023-05-28 21:45:56 +0600

    check all file

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
 import Home from '../pages/Home/Home'
+import Login from '../pages/Login/Login'
 
 
 export const router = createBrowserRouter([
@@ -15,4 +16,8 @@ export const router = createBrowserRouter([
     ]
 
   },
+  {
+    path: "/login",
+    element: <Login></Login>
+  }
 ])
